cab821f878f95ccd12eda91ffb5c5156c91b54bd
Kroes W. (Wouter) <wkr006@asr.nl>
2025-06-19 12:07:28 +0200

    add corrupt code example

--- apps/org/src/app/App.vue
+++ apps/org/src/app/App.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
-import NxWelcome from './NxWelcome.vue';
+import NxWelcome from "./NxWelcome.vue";
+
+interface Foo {
+  name: string;
+}
+
+const sendMail = async () => {
+  const mailData: Foo = {
+    step1: "wouter",
+  };
+
+  return mailData;
+};
+
+sendMail();
 </script>
 
 <template>
   <NxWelcome title="@org/org" />
 </template>
-
